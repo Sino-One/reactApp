0a55787317ae22f18c179f8bbf77e0363801c1ce
sinwan.meynaud <sinwan.meynaud@cbainfo.fr>
2022-08-03 15:21:54 +0200

    init store + reducer

---
+++ blog-react/src/redux/articles/articleReducer.js
@@ -0,0 +1,27 @@
+const INITIAL_STATE = {
+    articles: []
+}
+
+function articleReducer(state = INITIAL_STATE, action) {
+    switch(action.type) {
+        case "LOAD_ARTICLES": {
+            return {
+                ...state,
+                articles: action.payload
+            }
+        }
+    }
+    return state;
+}
+
+export default articleReducer;
+
+
+export const getArticles = () => dispatch => {
+    fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json()).then(data => {
+        dispatch({
+            type: 'LOAD_ARTICLES',
+            payload: data
+        })
+    })
+}
